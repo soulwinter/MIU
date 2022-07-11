61f5f02d246e7f2d6cbbe7bda2f9022decfd288c
Siaaa_3 <1059655438@qq.com>
2022-07-12 07:38:22 +0800

    修复了add_tag的bug

--- MyApplication/app/src/main/java/com/example/myapplication/fragments/ViewFragment.java
+++ MyApplication/app/src/main/java/com/example/myapplication/fragments/ViewFragment.java
@@ -8,7 +8,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.myapplication.AddTag;
